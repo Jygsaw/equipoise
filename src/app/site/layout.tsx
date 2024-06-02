b624dc7d2340b2100007a4a4e97a5c973fdb4060
Justin Cheung <jygsaw@gmail.com>
2024-06-02 02:44:40 -0700

    [NEW] Create site layout

---
+++ src/app/site/layout.tsx
@@ -0,0 +1,21 @@
+import { CopyrightLayout } from '@/app/site/_components/CopyrightLayout';
+
+import type { PropsWithChildren } from 'react';
+
+const Layout = ({ children }: Readonly<PropsWithChildren>) => {
+  return (
+    <CopyrightLayout>
+      <div className="h-full flex flex-col">
+        <header>HEADER</header>
+        <div className="w-screen flex-grow basis-0 flex">
+          <section>SIDEBAR</section>
+          <section className="flex-grow basis-0">
+            {children}
+          </section>
+        </div>
+      </div>
+    </CopyrightLayout>
+  );
+};
+
+export default Layout;
