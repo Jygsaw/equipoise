0a1ffc35ba40ecf94bda76a6b5da7244bf58017a
Justin Cheung <jygsaw@gmail.com>
2024-06-02 12:56:14 -0700

    [CHG] Refactor game index page to redirect

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,24 +1,7 @@
-import Link from 'next/link';
-import { titleAppend } from '@/app/(site)/_lib/siteUtils';
-
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: titleAppend('Splash'),
-};
+import { redirect } from 'next/navigation';
 
 const Page = () => {
-  return (
-    <main>
-      <p>SPLASH</p>
-
-      <nav>
-        <ul>
-          <li><Link href="/game/setup">Start</Link></li>
-        </ul>
-      </nav>
-    </main>
-  );
+  redirect('/game/splash');
 };
 
 export default Page;
