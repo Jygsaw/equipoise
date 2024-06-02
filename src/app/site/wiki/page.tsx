971d2a3cb7e18b80d4318151f78ceee18eb56fc4
Justin Cheung <jygsaw@gmail.com>
2024-06-02 03:11:57 -0700

    [NEW] Create site wiki home page

---
+++ src/app/site/wiki/page.tsx
@@ -0,0 +1,17 @@
+import { titleAppend } from '@/app/site/_lib/siteUtils';
+
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: titleAppend('Wiki'),
+};
+
+const Page = () => {
+  return (
+    <main>
+      <p>WIKI</p>
+    </main>
+  );
+};
+
+export default Page;
