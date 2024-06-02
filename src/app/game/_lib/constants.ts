a77411393286b4b8b99ad9ab588ec1abd41641c7
Justin Cheung <jygsaw@gmail.com>
2024-06-02 13:10:37 -0700

    [NEW] Create game constants

---
+++ src/app/game/_lib/constants.ts
@@ -0,0 +1,12 @@
+export const COLORS = {
+  black: 'black',
+  orange: 'orange',
+  white: 'white',
+} as const;
+
+export const SCREENS = {
+  play: 'play',
+  setup: 'setup',
+  splash: 'splash',
+  results: 'results',
+} as const;
