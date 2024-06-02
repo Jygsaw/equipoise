70e99b9baa2b34d3302fea7e4010b4b6c89c04df
Justin Cheung <jygsaw@gmail.com>
2024-06-02 02:06:56 -0700

    [NEW] Create CopyrightLayout

---
+++ src/app/site/_components/CopyrightLayout.tsx
@@ -0,0 +1,14 @@
+import { CopyrightFooter } from '@/app/site/_components/CopyrightFooter';
+
+import type { PropsWithChildren } from 'react';
+
+export const CopyrightLayout = ({ children }: Readonly<PropsWithChildren>) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="flex-grow basis-0">
+        {children}
+      </div>
+      <CopyrightFooter />
+    </div>
+  );
+};
