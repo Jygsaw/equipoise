871795e205c423b125642fa58c91e6a1b3dfdb79
Justin Cheung <jygsaw@gmail.com>
2024-06-02 03:11:44 -0700

    [NEW] Create site stats home page

---
+++ src/app/site/stats/page.tsx
@@ -0,0 +1,17 @@
+import { titleAppend } from '@/app/site/_lib/siteUtils';
+
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: titleAppend('Stats'),
+};
+
+const Page = () => {
+  return (
+    <main>
+      <p>STATS</p>
+    </main>
+  );
+};
+
+export default Page;
